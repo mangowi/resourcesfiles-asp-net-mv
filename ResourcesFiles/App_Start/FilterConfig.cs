b33cfa9e926a290bc68aac0f893eb4f7dc69c3a9
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-16 03:08:34 +0200

    Add project files.

---
+++ ResourcesFiles/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ResourcesFiles
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
